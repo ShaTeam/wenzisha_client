3d367b88d40134411e362c32231f729095d016a9
picacure <picacure@163.com>
2012-12-12 21:05:14 +0800

    today's work

--- init.js
+++ init.js
@@ -1,47 +1,6 @@
 (function($,window,Hammer){
 
-    //window.Notification.simple("hello");
-
     localStorage.clear();
-
-//    var $stripUl = $('.strip ul'),
-//        tb = ["0px","-270px","-540px","-810px","-980px","-1250px"],
-//        cIndex = 0
-//        ;
-//
-//    var hammer = new Hammer(document.querySelector('#tips'));
-//
-//    hammer.onswipe = function(ex){
-//        if(ex.direction == 'right'){
-//            (cIndex >= 1) ? cIndex-- : '';
-//            $stripUl.animate(
-//                { marginLeft: tb[cIndex] },
-//                .3,
-//                'ease-in-out',
-//                function(){
-//
-//                }
-//            )
-//        }
-//        else{
-//            (cIndex < 4) ? cIndex++ : '';
-//            $stripUl.animate(
-//                { marginLeft: tb[cIndex] },
-//                .3,
-//                'ease-in-out',
-//                function(){
-//
-//                }
-//            )
-//        }
-//
-//        ex.stopPropagation();
-//
-//    }
-//
-//    hammer.ontap = function(ex){
-//        alert("tap");
-//    }
 
     $('.slider').slider({ margin: 5, isPlay: false, lazy : 'data-img'});
 
@@ -55,6 +14,21 @@
         e.preventDefault();
         e.stopPropagation();
         $('.card').addClass('hide');
+    });
+
+    //出题.
+    $('.pen').on('tap',function(e){
+
+
+        $('header').removeClass('.h-tall').addClass('h-comm')
+    });
+
+    $('#set').on('tap',function(e){
+        alert('set');
+    });
+
+    $('.btn').on('tap',function(e){
+        alert('btn');
     });
 
 })(Zepto,window,Hammer);
